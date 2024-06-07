099a438cf7cbdfd6cbc4a082574b04e957ddf545
Trung <105847960+xuantrungtrinh@users.noreply.github.com>
2024-06-07 10:20:30 +0700

    Update debugging exercises.py

--- 04 debugging/debugging exercises.py
+++ 04 debugging/debugging exercises.py
@@ -80,6 +80,21 @@ else:
 
 --------------------------------------------
 
+# # Which year do you want to check?
+# year = input()
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+  
+# ==> solution (debugged):
 # Which year do you want to check?
 year = int(input())
 
