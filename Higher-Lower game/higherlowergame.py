ddff96c2d7ad3d59c5c7e5aacf49d10ed52abc6a
Trung <105847960+xuantrungtrinh@users.noreply.github.com>
2024-06-07 11:26:50 +0700

    Update higherlowergame.py

--- Higher-Lower game/higherlowergame.py
+++ Higher-Lower game/higherlowergame.py
@@ -1,3 +1,4 @@
 # Project: you're given a dictionary ... different people's personal information and their Instagram following count. Your task is to generate a program that compares 2 random people's follower count in this dictionary. 
 
-# If the use 
+# If the user chooses correctly, he gains 1 additional point and continues choosing.
+# If the user chooses incorrectly, the game ends.
