5e20c81787d9aef92c3b46f817092a403f06249b
Trung <105847960+xuantrungtrinh@users.noreply.github.com>
2024-06-07 10:29:48 +0700

    Update debugging exercises.py

--- 04 debugging/debugging exercises.py
+++ 04 debugging/debugging exercises.py
@@ -109,5 +109,28 @@ if year % 4 == 0:
 else:
   print("Not leap year.")
   
+--------------------------------------------
 
+# target = int(input())
+# for number in range(1, target + 1):
+#   if number % 3 == 0 or number % 5 == 0:
+#     print("FizzBuzz")
+#   if number % 3 == 0:
+#     print("Fizz")
+#   if number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print([number])
+
+# ==> Solution: 
+target = int(input())
+for number in range(1, target + 1):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
 
