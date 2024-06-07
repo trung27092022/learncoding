7b062e65fb57874a176e72b9c0b471827bab6ed3
Trung <105847960+xuantrungtrinh@users.noreply.github.com>
2024-06-07 11:24:40 +0700

    Update higherlowergame.py

--- Higher-Lower game/higherlowergame.py
+++ Higher-Lower game/higherlowergame.py
@@ -1,1 +1,3 @@
+# Project: you're given a dictionary ... different people's personal information and their Instagram following count. Your task is to generate a program that compares 2 random people's follower count in this dictionary. 
 
+# If the use 
