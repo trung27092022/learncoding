591e7ec5320d7e1e6dfa1fb28826efeee005d255
Trung <105847960+xuantrungtrinh@users.noreply.github.com>
2024-07-02 04:34:46 +0700

    Add files via upload

---
+++ [18] Turtle & GUI (graphical user interface)/Challenge 1: creating a simple turtle square/main.py
@@ -0,0 +1,26 @@
+#####Turtle Intro######
+
+import turtle as t
+from turtle import Screen
+
+timmy_the_turtle = t.Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("black")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+# timmy_the_turtle.backward(200)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+# timmy_the_turtle.left(180)
+# timmy_the_turtle.setheading(0)
+
+screen = Screen()
+screen.exitonclick()
+
+
+
+
