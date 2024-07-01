64ba9539fed78ab12fc070d0a8badb97e6b993d7
Trung <105847960+xuantrungtrinh@users.noreply.github.com>
2024-07-01 07:28:51 +0700

    Add files via upload

---
+++ [17] creating custom class in OOP Python/the quiz/Part 5: Checking Answers and Keeping Score/quiz/question_model.py
@@ -0,0 +1,18 @@
+# Part 1: Quiz Project Part 1: Creating the Question Class (from scratch)
+class Question:
+    def __init__(self, question, answer):
+        self.enterquestion = question
+        self.theanswer = answer
+
+# question1 = Question("2+3", "True")
+
+# print(question1.theanswer)
+
+
+
+
+
+
+
+
+
